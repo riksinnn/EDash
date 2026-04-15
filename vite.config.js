c2c218d4deb98f20bde8e2190b0aba417f35d0fd
jbitterz <your_email_for_jbitterz@example.com>
2026-04-15 17:47:50 +0800

    remove pwa fully

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,10 @@
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
-import { VitePWA } from 'vite-plugin-pwa'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import tailwindcss from "@tailwindcss/vite"
 
-export default {
+export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(),
-    VitePWA({ 
-      registerType: 'autoUpdate',
-      manifest: {
-        name: 'Edash Planner',
-        short_name: 'Edash',
-        theme_color: '#556B2F',
-        background_color: '#F5F5F0',
-        display: 'standalone',
-      }
-    })
+    tailwindcss()
   ]
-}
+})
