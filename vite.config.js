96a009b11d2f544994f5f5a734003d4f3a40c647
jbitterz <your_email_for_jbitterz@example.com>
2026-04-21 12:26:36 +0800

    index update

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,6 @@ import react from "@vitejs/plugin-react"
 import tailwindcss from "@tailwindcss/vite"
 
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      external: ["react-day-picker"],
-    },
-  },
   plugins: [
     react(),
     tailwindcss()
